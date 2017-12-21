d890f87009b11385f3aef7836b290bc520c03b84
Florian Fontan <dev@florian-fontan.fr>
2017-12-21 21:05:59 +0100

    Implement extra Dantzig upper bounds

--- ub_dantzig/dantzig.cpp
+++ ub_dantzig/dantzig.cpp
@@ -27,11 +27,13 @@ Profit ub_dantzig(const Instance& instance)
 	return p;
 }
 
-Profit ub_dantzig_without(const Instance& instance, ItemIdx j, Weight capacity)
+#undef DBG
+
+Profit ub_dantzig_without(const Instance& instance, ItemIdx j, Weight c)
 {
 	ItemIdx i = 1;
 	Profit p = 0;
-	Weight remaining_capacity = capacity;
+	Weight remaining_capacity = c;
 	for (i=1; i<=instance.item_number(); i++) {
 		if (i == j)
 			continue;
@@ -46,11 +48,11 @@ Profit ub_dantzig_without(const Instance& instance, ItemIdx j, Weight capacity)
 	return p;
 }
 
-Profit ub_dantzig_from(const Instance& instance, ItemIdx j, Weight capacity)
+Profit ub_dantzig_from(const Instance& instance, ItemIdx j, Weight c)
 {
 	ItemIdx i = 1;
 	Profit p = 0;
-	Weight remaining_capacity = capacity;
+	Weight remaining_capacity = c;
 	for (i=j; i<=instance.item_number(); i++) {
 		Weight wi = instance.weight(i);
 		if (wi > remaining_capacity)
@@ -60,6 +62,51 @@ Profit ub_dantzig_from(const Instance& instance, ItemIdx j, Weight capacity)
 	}
 	if (i != instance.item_number() + 1 && remaining_capacity > 0)
 		p += (instance.profit(i) * remaining_capacity) / instance.weight(i);
+	return p;
+}
+
+Profit ub_dantzig_from_to(const Instance& instance, ItemIdx i1, ItemIdx i2, Weight c)
+{
+	ItemIdx i = i1;
+	Profit  p = 0;
+	Weight  r = c;
+	for (; i<=i2; i++) {
+		Weight wi = instance.weight(i);
+		if (wi > r)
+			break;
+		p += instance.profit(i);
+		r -= wi;
+	}
+	if (i != i2 + 1 && r > 0)
+		p += (instance.profit(i) * r) / instance.weight(i);
+	return p;
+}
+
+#define DBG(x)
+//#define DBG(x) x
+
+Profit ub_dantzig_except(const Instance& instance,
+		ItemIdx n1, ItemIdx i1, ItemIdx i2, ItemIdx n2, Weight c)
+{
+	DBG(std::cout << "ub_dantzig_except " << n1 << " " << i1 << " " << i2 << " " << n2 << " " << c << std::endl;)
+	ItemIdx i = n1;
+	if (i == i1)
+		i = i2+1;
+	Profit  p = 0;
+	Weight remaining_capacity = c;
+	for (; i<=n2; i++) {
+		DBG(std::cout << "i " << i << std::endl;)
+		Weight wi = instance.weight(i);
+		if (wi > remaining_capacity)
+			break;
+		p += instance.profit(i);
+		remaining_capacity -= wi;
+		if (i == i1-1)
+			i = i2;
+	}
+	if (i != instance.item_number() + 1 && remaining_capacity > 0)
+		p += (instance.profit(i) * remaining_capacity) / instance.weight(i);
+	DBG(std::cout << "p " << p << std::endl;)
 	return p;
 }
 
--- ub_dantzig/dantzig.hpp
+++ ub_dantzig/dantzig.hpp
@@ -5,7 +5,26 @@
 #include "../lib/solution.hpp"
 
 Profit ub_dantzig(const Instance& instance);
-Profit ub_dantzig_from(const Instance& instance, ItemIdx i, Weight c);
-Profit ub_dantzig_without(const Instance& instance, ItemIdx i, Weight c);
+
+/**
+ * Dantzig upper bound, using items j..n
+ */
+Profit ub_dantzig_from(const Instance& instance, ItemIdx j, Weight c);
+
+/**
+ * Dantzig upper bound, using items i1..i2
+ */
+Profit ub_dantzig_from_to(const Instance& instance, ItemIdx i1, ItemIdx i2, Weight c);
+
+/**
+ * Dantzig upper bound, using all items except j
+ */
+Profit ub_dantzig_without(const Instance& instance, ItemIdx j, Weight c);
+
+/**
+ * Dantzig upper bound, using items n1..i1-1,i2+1..n2
+ */
+Profit ub_dantzig_except(const Instance& instance,
+		ItemIdx n1, ItemIdx i1, ItemIdx i2, ItemIdx n2, Weight c);
 
 #endif /* end of include guard: DANTZIG_HPP_6HA0XAKO */
