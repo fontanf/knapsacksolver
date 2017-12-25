715c522fd88e02718a12a62e34aaa4e3b7cf370c
Florian Fontan <dev@florian-fontan.fr>
2017-12-25 13:48:48 +0100

    Implement local search

---
+++ lb_ls/main.cpp
@@ -0,0 +1,59 @@
+#include "ls.hpp"
+
+#include <iostream>
+
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/ini_parser.hpp>
+#include <boost/filesystem/operations.hpp>
+#include <boost/filesystem/path.hpp>
+#include <boost/program_options.hpp>
+
+int main(int argc, char *argv[])
+{
+	// Parse program options
+	std::string input_data  = "";
+	std::string output_file = "";
+	std::string cert_file   = "";
+	std::string algorithm   = "";
+	boost::program_options::options_description desc("Allowed options");
+	desc.add_options()
+		("help,h", "produce help message")
+		("input-data,i",  boost::program_options::value<std::string>(&input_data)->required(), "set input data (required)")
+		("output-file,o", boost::program_options::value<std::string>(&output_file),            "set output file")
+		("cert-file,c",   boost::program_options::value<std::string>(&cert_file),              "set certificate output file")
+		("verbose,v",                                                                          "enable verbosity")
+		;
+	boost::program_options::variables_map vm;
+	boost::program_options::store(boost::program_options::parse_command_line(argc, argv, desc), vm);
+	if (vm.count("help")) {
+		std::cout << desc << std::endl;;
+		return 1;
+	}
+	try {
+		boost::program_options::notify(vm);
+	} catch (boost::program_options::required_option e) {
+		std::cout << desc << std::endl;;
+		return 1;
+	}
+	bool verbose = vm.count("verbose");
+
+	Instance instance(input_data);
+	Instance instance_sorted = Instance::sort_by_efficiency(instance);
+	Solution solution(instance);
+	boost::property_tree::ptree pt;
+	solution = sol_ls(instance_sorted, -1, &pt, verbose).get_orig();
+
+	// Write output file
+	if (output_file != "")
+		write_ini(output_file, pt);
+
+	// Write certificate file
+	if (cert_file != "") {
+		std::ofstream cert;
+		cert.open(cert_file);
+		cert << solution;
+		cert.close();
+	}
+
+	return 0;
+}
