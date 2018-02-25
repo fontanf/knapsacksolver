1ec3d51d076f02f8f76ffefd6d61c90bcc6f3b94
Florian Fontan <dev@florian-fontan.fr>
2018-02-25 10:24:04 +0300

    Update tabs to spaces

--- opt_babprimaldual/main.cpp
+++ opt_babprimaldual/main.cpp
@@ -10,64 +10,64 @@
 
 int main(int argc, char *argv[])
 {
-	// Parse program options
-	std::string input_data  = "";
-	std::string output_file = "";
-	std::string cert_file   = "";
-	std::string algorithm   = "";
-	boost::program_options::options_description desc("Allowed options");
-	desc.add_options()
-		("help,h", "produce help message")
-		("input-data,i",  boost::program_options::value<std::string>(&input_data)->required(), "set input data (required)")
-		("output-file,o", boost::program_options::value<std::string>(&output_file),            "set output file")
-		("cert-file,c",   boost::program_options::value<std::string>(&cert_file),              "set certificate output file")
-		("algorithm,a",   boost::program_options::value<std::string>(&algorithm),              "set algorithm")
-		("verbose,v",                                                                          "enable verbosity")
-		;
-	boost::program_options::variables_map vm;
-	boost::program_options::store(boost::program_options::parse_command_line(argc, argv, desc), vm);
-	if (vm.count("help")) {
-		std::cout << desc << std::endl;;
-		return 1;
-	}
-	try {
-		boost::program_options::notify(vm);
-	} catch (boost::program_options::required_option e) {
-		std::cout << desc << std::endl;;
-		return 1;
-	}
-	bool verbose = vm.count("verbose");
+    // Parse program options
+    std::string input_data  = "";
+    std::string output_file = "";
+    std::string cert_file   = "";
+    std::string algorithm   = "";
+    boost::program_options::options_description desc("Allowed options");
+    desc.add_options()
+        ("help,h", "produce help message")
+        ("input-data,i",  boost::program_options::value<std::string>(&input_data)->required(), "set input data (required)")
+        ("output-file,o", boost::program_options::value<std::string>(&output_file),            "set output file")
+        ("cert-file,c",   boost::program_options::value<std::string>(&cert_file),              "set certificate output file")
+        ("algorithm,a",   boost::program_options::value<std::string>(&algorithm),              "set algorithm")
+        ("verbose,v",                                                                          "enable verbosity")
+        ;
+    boost::program_options::variables_map vm;
+    boost::program_options::store(boost::program_options::parse_command_line(argc, argv, desc), vm);
+    if (vm.count("help")) {
+        std::cout << desc << std::endl;;
+        return 1;
+    }
+    try {
+        boost::program_options::notify(vm);
+    } catch (boost::program_options::required_option e) {
+        std::cout << desc << std::endl;;
+        return 1;
+    }
+    bool verbose = vm.count("verbose");
 
-	Instance instance(input_data);
-	boost::property_tree::ptree pt;
-	Solution sol_orig(instance);
+    Instance instance(input_data);
+    boost::property_tree::ptree pt;
+    Solution sol_orig(instance);
 
-	if (algorithm == "partsorted") {
-		Instance instance_sorted = Instance::sort_partially_by_efficiency(instance);
-		BabPDData data(instance_sorted, &pt, verbose);
-		sopt_babprimaldual(data);
-		sol_orig = data.sol_best.get_orig();
-	} else if (algorithm == "sorted") {
-		Instance instance_sorted = Instance::sort_by_efficiency(instance);
-		BabPDData data(instance_sorted, &pt, verbose);
-		sopt_babprimaldual(data);
-		sol_orig = data.sol_best.get_orig();
-	} else {
-		assert(false);
-		std::cout << "Unknwow algorithm" << std::endl;
-	}
+    if (algorithm == "partsorted") {
+        Instance instance_sorted = Instance::sort_partially_by_efficiency(instance);
+        BabPDData data(instance_sorted, &pt, verbose);
+        sopt_babprimaldual(data);
+        sol_orig = data.sol_best.get_orig();
+    } else if (algorithm == "sorted") {
+        Instance instance_sorted = Instance::sort_by_efficiency(instance);
+        BabPDData data(instance_sorted, &pt, verbose);
+        sopt_babprimaldual(data);
+        sol_orig = data.sol_best.get_orig();
+    } else {
+        assert(false);
+        std::cout << "Unknwow algorithm" << std::endl;
+    }
 
-	// Write output file
-	if (output_file != "")
-		write_ini(output_file, pt);
+    // Write output file
+    if (output_file != "")
+        write_ini(output_file, pt);
 
-	// Write certificate file
-	if (cert_file != "") {
-		std::ofstream cert;
-		cert.open(cert_file);
-		cert << sol_orig;
-		cert.close();
-	}
+    // Write certificate file
+    if (cert_file != "") {
+        std::ofstream cert;
+        cert.open(cert_file);
+        cert << sol_orig;
+        cert.close();
+    }
 
-	return 0;
+    return 0;
 }
