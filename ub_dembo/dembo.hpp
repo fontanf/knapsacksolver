2edf558221a0380c97f98f2efd80ddcf67ca64bc
Florian Fontan <dev@florian-fontan.fr>
2018-03-14 08:07:08 +0300

    Add UB package

---
+++ ub_dembo/dembo.hpp
@@ -0,0 +1,83 @@
+#pragma once
+
+#include "../lib/instance.hpp"
+#include "../lib/solution.hpp"
+
+Profit ub_0(const Instance& ins, ItemPos j, const Solution& sol_curr);
+Profit ub_0(const Instance& ins, ItemPos j, Profit p, Weight r);
+Profit ub_0_rev(const Instance& ins, ItemPos j, const Solution& sol_curr);
+Profit ub_0_rev(const Instance& ins, ItemPos j, Profit p, Weight r);
+
+/**
+ * u = p + r * pj / wj
+ * Time  O(1)
+ * Space O(1)
+ * - rev: for unfeasible solution
+ */
+Profit ub_dembo(const Instance& ins, ItemPos j, const Solution& sol_curr);
+Profit ub_dembo(const Instance& ins, ItemPos j, Profit p, Weight r);
+Profit ub_dembo_rev(const Instance& ins, ItemPos j, const Solution& sol_curr);
+Profit ub_dembo_rev(const Instance& ins, ItemPos j, Profit p, Weight r);
+
+
+/******************************************************************************/
+
+#define DBG(x)
+//#define DBG(x) x
+
+inline Profit ub_0(const Instance& ins, ItemIdx j, const Solution& sol_curr)
+{
+    Profit u = ub_0(ins, j, sol_curr.profit(), sol_curr.remaining_capacity());
+    return u;
+}
+
+inline Profit ub_0(const Instance& ins, ItemIdx j, Profit p, Weight r)
+{
+    if (j < ins.item_number())
+        p += (r * ins.most_efficient_item().p) / ins.most_efficient_item().w;
+    return p;
+}
+
+inline Profit ub_0_rev(const Instance& ins, ItemIdx j, const Solution& sol_curr)
+{
+    return ub_0_rev(ins, j, sol_curr.profit(), sol_curr.remaining_capacity());
+}
+
+inline Profit ub_0_rev(const Instance& ins, ItemIdx j, Profit p, Weight r)
+{
+    if (j >= 0)
+        p += (r * ins.most_efficient_item().p + 1) / ins.most_efficient_item().w - 1;
+    return p;
+}
+
+/******************************************************************************/
+
+inline Profit ub_dembo(const Instance& ins, ItemIdx j, const Solution& sol_curr)
+{
+    Profit u = ub_dembo(ins, j, sol_curr.profit(), sol_curr.remaining_capacity());
+    return u;
+}
+
+inline Profit ub_dembo(const Instance& ins, ItemIdx j, Profit p, Weight r)
+{
+    if (j < ins.item_number())
+        p += (r * ins.item(j).p) / ins.item(j).w;
+    return p;
+}
+
+inline Profit ub_dembo_rev(const Instance& ins, ItemIdx j, const Solution& sol_curr)
+{
+    return ub_dembo_rev(ins, j, sol_curr.profit(), sol_curr.remaining_capacity());
+}
+
+inline Profit ub_dembo_rev(const Instance& ins, ItemIdx j, Profit p, Weight r)
+{
+    DBG(std::cout << "UBTRIVIALFROMREV... j " << j << std::endl;)
+    if (j >= 0)
+        p += (r * ins.item(j).p + 1) / ins.item(j).w - 1;
+    DBG(std::cout << "UBTRIVIALFROMREV... END" << std::endl;)
+    return p;
+}
+
+#undef DBG
+
