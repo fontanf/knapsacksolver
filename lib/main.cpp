53f15b9d4fb274337c6cac956da11dc26b25bd90
Florian Fontan <dev@florian-fontan.fr>
2018-11-19 12:27:30 +0100

    Update main

--- lib/main.cpp
+++ lib/main.cpp
@@ -1,9 +1,9 @@
-//#include "knapsack/opt_bab/bab.hpp"
+#include "knapsack/opt_bab/bab.hpp"
 #include "knapsack/opt_babstar/bab.hpp"
-//#include "knapsack/opt_bellman/bellman_list.hpp"
-//#include "knapsack/opt_bellman/bellman_array.hpp"
-//#include "knapsack/opt_expknap/expknap.hpp"
-//#include "knapsack/opt_balknap/balknap.hpp"
+#include "knapsack/opt_bellman/bellman_list.hpp"
+#include "knapsack/opt_bellman/bellman_array.hpp"
+#include "knapsack/opt_expknap/expknap.hpp"
+#include "knapsack/opt_balknap/balknap.hpp"
 #include "knapsack/opt_minknap/minknap.hpp"
 
 #include "knapsack/lib/generator.hpp"
@@ -19,28 +19,35 @@ int main(int argc, char *argv[])
         //for (ItemIdx i=10; i<50; i++) {
             //for (Weight r=5; r<=200; ++r) {
                 //for (int h=1; h<=100; ++h) {
-                    std::string t = "sc";
-                    ItemIdx i = 30;
-                    Weight r = 100;
-                    int h = 50;
+                    std::string t = "wc";
+                    ItemIdx i = 20;
+                    Weight r = 50;
+                    int h = 41;
                     std::cout << t << " " << i << " " << r << " " << h << std::endl;
                     std::cout << std::endl;
                     Instance ins = generate(t, i, r, h);
-                    //std::cout << ins << std::endl;
+                    std::cout << ins << std::endl;
 
                     Instance ins1(ins);
-                    Info info1(true);
+                    Info info1;
+                    info1.set_verbose();
                     Solution sopt1 = sopt_minknap_list_part(ins1, info1);
+                    std::cout << sopt1.print_bin() << std::endl;
+                    std::cout << sopt1.print_in() << std::endl;
 
                     std::cout << std::endl;
 
                     Instance ins2(ins);
-                    Info info2(true, false, false);
-                    Solution sopt2 = sopt_babstar_dp(ins2, info2);
-                    if (sopt1.profit() != sopt2.profit()) {
-                        std::cout << info2.debug_string << std::endl;
-                        return 0;
-                    }
+                    Info info2;
+                    info2.set_verbose();
+                    info2.set_debug();
+                    info2.set_debuglive();
+                    //Solution sopt2 = sopt_babstar_dp(ins2, info2);
+                    Solution sopt2 = sopt_balknap_list_all(ins2, info2);
+                    //if (sopt1.profit() != sopt2.profit()) {
+                        //std::cout << info2.debug_string << std::endl;
+                        //return 0;
+                    //}
 
                     std::cout << std::endl;
                     std::cout << std::endl;
